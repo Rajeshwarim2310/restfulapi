d6967bad5d4d45f3f4394c803cf5295a814a463c
unknown <rajimuthu2310@gmail.com>
2023-05-16 14:24:24 +0530

    test

---
+++ restfulproject/Program.cs
@@ -0,0 +1,38 @@
+using restfulproject.Models.EF;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddTransient(typeof(restfulproject         .Models.EF.ShoppingDbNikhilContext));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(cors =>
+{
+    cors.AddDefaultPolicy(defaultpolicy =>
+    {
+        defaultpolicy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+    //cors.AddPolicy("vendorA", vendorpolicy =>
+    //{
+    //    vendorpolicy.WithOrigins(new string[] { "https://www.transportmanagement.com","https://www.cognizantHR"})
+    //})
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
